86d7b8c50c5baa8e87dd37d7ba86c7c7bf0c5f68
sersmirnov <sersmirnov@yandex-team.ru>
2020-05-24 00:37:15 +0300

    Updates

---
+++ precache-manifest.6f958570e98ce419240d66d64e5d46df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebad4ce8eab63b23d6acc2078ff1aa5c",
+    "url": "/DSS/index.html"
+  },
+  {
+    "revision": "908c3768f42ff8a92f64",
+    "url": "/DSS/static/css/main.179f2301.chunk.css"
+  },
+  {
+    "revision": "687db0f15dc82df15a15",
+    "url": "/DSS/static/js/2.f426a9e3.chunk.js"
+  },
+  {
+    "revision": "43cea629485599e3dffb3092006586d7",
+    "url": "/DSS/static/js/2.f426a9e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908c3768f42ff8a92f64",
+    "url": "/DSS/static/js/main.954cec83.chunk.js"
+  },
+  {
+    "revision": "fb8995a78896df62c624",
+    "url": "/DSS/static/js/runtime-main.207add72.js"
+  }
+]);
